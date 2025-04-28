c14663ad24abb905e9b01239237e9228c6a525e7
nzm <nzm6846@gmail.com>
2025-04-28 14:33:51 +0900

    feat: 리뷰매퍼 추가 config변경

---  Library_Management/src/main/java/kr/or/ddit/books/controller/ReviewListController.java
+++  Library_Management/src/main/java/kr/or/ddit/books/controller/ReviewListController.java
@@ -1,20 +1,28 @@
 package kr.or.ddit.books.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kr.or.ddit.books.service.BooksServiceImp;
+import kr.or.ddit.books.service.IBooksService;
+import kr.or.ddit.vo.ReviewsVo;
 
 @WebServlet("/books/detail/reviewList")
 public class ReviewListController extends HttpServlet{
+	IBooksService booksService = BooksServiceImp.getInsatance();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String bookBoStr = req.getParameter("bookNo");
 		int bookNo = Integer.parseInt(bookBoStr);
-		System.out.println("aaaa"+bookNo);
+		
+		List<ReviewsVo>reviewList =  booksService.reviewList(bookNo);
+		
 	}
 }
